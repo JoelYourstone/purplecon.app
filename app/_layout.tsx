61febe9e5266c9e0de7f9dbdc0107ac07464e55d
JoelYourstone <joel.yourstone@gmail.com>
2025-03-13 20:49:54 +0100

    split layout and AppProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,5 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { differenceInMinutes } from "date-fns";
-import { usePathname, useRouter } from "expo-router";
 import { Stack } from "expo-router/stack";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
 import { Platform, useColorScheme } from "react-native";
-import { setBackgroundColorAsync } from "expo-system-ui";
-import { ActionSheetProvider } from "@expo/react-native-action-sheet";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as Notifications from "expo-notifications";
 
 import { theme } from "../theme";
@@ -19,7 +7,7 @@ import { theme } from "../theme";
 import { BackButton } from "@/components/BackButton";
 import { OfflineBanner } from "@/components/OfflineBanner";
 import { ThemedText, useThemeColor } from "@/components/Themed";
-import { AnimatedBootSplash } from "@/components/AnimatedBootSplash";
+import { AppProvider } from "@/components/AppProvider";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -30,130 +18,69 @@ Notifications.setNotificationHandler({
 });
 
 export default function Layout() {
-  const [splashVisible, setSplashVisible] = useState(true);
-  const router = useRouter();
-  const pathName = usePathname();
   const colorScheme = useColorScheme() || "light";
-
   const tabBarBackgroundColor = useThemeColor({
     light: theme.colorWhite,
     dark: theme.colorDarkestBlue,
   });
 
-  // Keep the root view background color in sync with the current theme
-  useEffect(() => {
-    setBackgroundColorAsync(
-      colorScheme === "dark" ? theme.colorDarkestBlue : theme.colorWhite
-    );
-  }, [colorScheme]);
-
-  const lastNotificationResponse = Notifications.useLastNotificationResponse();
-  useEffect(() => {
-    if (
-      lastNotificationResponse &&
-      lastNotificationResponse.actionIdentifier ===
-        Notifications.DEFAULT_ACTION_IDENTIFIER
-    ) {
-      try {
-        const url =
-          lastNotificationResponse.notification.request.content.data.url;
-        if (pathName !== url) {
-          router.push(url);
-        }
-      } catch {}
-    }
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lastNotificationResponse]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if (
-        true
-        // !lastRefreshed ||
-        // differenceInMinutes(new Date(), new Date(lastRefreshed)) > 5
-      ) {
-        // await refreshData();
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ActionSheetProvider>
-        <AnimatedBootSplash
-          animationEnded={!splashVisible}
-          onAnimationEnd={() => {
-            setSplashVisible(false);
+    <AppProvider>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
           }}
-        >
-          <ThemeProvider
-            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-          >
-            <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
-
-            <Stack>
-              <Stack.Screen
-                name="(tabs)"
-                options={{
-                  headerShown: false,
-                }}
-              />
-              <Stack.Screen
-                name="talk/[talkId]"
-                options={{
-                  headerLeft: () =>
-                    Platform.OS === "ios" ? <BackButton /> : null,
-                  title: "",
-                  headerTransparent: true,
-                  // `headerBlurEffect` prop does not work on New Architecture at the moment
-                  // headerBlurEffect: "systemUltraThinMaterialLight",
-                  presentation: "modal",
-                }}
-              />
-              <Stack.Screen
-                name="speaker/[speakerId]"
-                options={{
-                  presentation: "modal",
-                  headerLeft: () =>
-                    Platform.OS === "ios" ? <BackButton /> : null,
-                  headerStyle: {
-                    backgroundColor: tabBarBackgroundColor,
-                  },
-                  headerTitleAlign: "center",
-                  headerTitle: (props) => (
-                    <ThemedText fontSize={24} fontWeight="bold">
-                      {props.children}
-                    </ThemedText>
-                  ),
-                }}
-              />
-              <Stack.Screen
-                name="secretModal"
-                options={{
-                  presentation: "modal",
-                  title: "Secret Modal",
-                  headerTitleAlign: "center",
-                  headerTitle: (props) => (
-                    <ThemedText fontSize={24} fontWeight="bold">
-                      {props.children}
-                    </ThemedText>
-                  ),
-                  ...(colorScheme === "dark"
-                    ? {
-                        headerStyle: { backgroundColor: theme.colorDarkBlue },
-                        headerTitleStyle: { color: "white" },
-                      }
-                    : {}),
-                }}
-              />
-            </Stack>
-            <OfflineBanner />
-          </ThemeProvider>
-        </AnimatedBootSplash>
-      </ActionSheetProvider>
-    </GestureHandlerRootView>
+        />
+        <Stack.Screen
+          name="talk/[talkId]"
+          options={{
+            headerLeft: () => (Platform.OS === "ios" ? <BackButton /> : null),
+            title: "",
+            headerTransparent: true,
+            // `headerBlurEffect` prop does not work on New Architecture at the moment
+            // headerBlurEffect: "systemUltraThinMaterialLight",
+            presentation: "modal",
+          }}
+        />
+        <Stack.Screen
+          name="speaker/[speakerId]"
+          options={{
+            presentation: "modal",
+            headerLeft: () => (Platform.OS === "ios" ? <BackButton /> : null),
+            headerStyle: {
+              backgroundColor: tabBarBackgroundColor,
+            },
+            headerTitleAlign: "center",
+            headerTitle: (props) => (
+              <ThemedText fontSize={24} fontWeight="bold">
+                {props.children}
+              </ThemedText>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="secretModal"
+          options={{
+            presentation: "modal",
+            title: "Secret Modal",
+            headerTitleAlign: "center",
+            headerTitle: (props) => (
+              <ThemedText fontSize={24} fontWeight="bold">
+                {props.children}
+              </ThemedText>
+            ),
+            ...(colorScheme === "dark"
+              ? {
+                  headerStyle: { backgroundColor: theme.colorDarkBlue },
+                  headerTitleStyle: { color: "white" },
+                }
+              : {}),
+          }}
+        />
+      </Stack>
+      <OfflineBanner />
+    </AppProvider>
   );
 }
---
+++ components/AppProvider.tsx
@@ -0,0 +1,89 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { usePathname, useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import { setBackgroundColorAsync } from "expo-system-ui";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as Notifications from "expo-notifications";
+
+import { theme } from "../theme";
+
+import { OfflineBanner } from "@/components/OfflineBanner";
+import { AnimatedBootSplash } from "@/components/AnimatedBootSplash";
+import { PropsWithChildren } from "react";
+
+export function AppProvider({ children }: PropsWithChildren) {
+  const [splashVisible, setSplashVisible] = useState(true);
+  const colorScheme = useColorScheme() || "light";
+
+  const router = useRouter();
+  const pathName = usePathname();
+
+  // Keep the root view background color in sync with the current theme
+  useEffect(() => {
+    setBackgroundColorAsync(
+      colorScheme === "dark" ? theme.colorDarkestBlue : theme.colorWhite
+    );
+  }, [colorScheme]);
+
+  const lastNotificationResponse = Notifications.useLastNotificationResponse();
+  useEffect(() => {
+    if (
+      lastNotificationResponse &&
+      lastNotificationResponse.actionIdentifier ===
+        Notifications.DEFAULT_ACTION_IDENTIFIER
+    ) {
+      try {
+        const url =
+          lastNotificationResponse.notification.request.content.data.url;
+        if (pathName !== url) {
+          router.push(url);
+        }
+      } catch {}
+    }
+    // eslint-disable-next-line react-compiler/react-compiler
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastNotificationResponse]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (
+        true
+        // !lastRefreshed ||
+        // differenceInMinutes(new Date(), new Date(lastRefreshed)) > 5
+      ) {
+        // await refreshData();
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ActionSheetProvider>
+        <AnimatedBootSplash
+          animationEnded={!splashVisible}
+          onAnimationEnd={() => {
+            setSplashVisible(false);
+          }}
+        >
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+
+            {children}
+            <OfflineBanner />
+          </ThemeProvider>
+        </AnimatedBootSplash>
+      </ActionSheetProvider>
+    </GestureHandlerRootView>
+  );
+}
