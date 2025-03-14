087f5d62cdbe71460208266bf1e27db3f720c416
JoelYourstone <joel.yourstone@gmail.com>
2025-03-14 08:34:01 +0100

    Add db types for supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,4 @@
+import { Database } from "@/supabase";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
@@ -5,7 +6,7 @@ const supabaseUrl = "https://ljjhtynzegxittmgmdvd.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxqamh0eW56ZWd4aXR0bWdtZHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NTM0ODMsImV4cCI6MjA1NzAyOTQ4M30.noDqqiEJ-Uly7h0PUFUDHNTXL3KEw4Oudv9rRWEtZmc";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
