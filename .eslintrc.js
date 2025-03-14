839c2a7297d721ac46679959f21bc4bfd0d33bbc
JoelYourstone <joel.yourstone@gmail.com>
2025-03-14 21:39:28 +0100

    Fix prettier and eslint bugs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@
 module.exports = {
   extends: ["expo", "prettier"],
   plugins: ["prettier", "eslint-plugin-react-compiler"],
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
   rules: {
     "prettier/prettier": "error",
     "react-compiler/react-compiler": "error",
